2b43bcd0f39ff1cc7682bbe479e2f4c8378013dc
KendaroDuong <kendaro@vt.edu>
2024-08-27 17:45:36 -0400

    I changed the output of the first line to "Hello programing!" and added another line of code to add "Here we go!".

---
+++ build/CMakeFiles/CMakeSetUp.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CMakeSetUp.dir/main.cpp.obj"
+  "CMakeFiles/CMakeSetUp.dir/main.cpp.obj.d"
+  "CMakeSetUp.exe"
+  "CMakeSetUp.exe.manifest"
+  "CMakeSetUp.pdb"
+  "libCMakeSetUp.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CMakeSetUp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
